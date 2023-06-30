76967b61715c203cf481924c045719a76d8865a7
irakli2206 <iraklibego1@gmail.com>
2023-06-30 20:15:50 +0400

    changed eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,9 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "prefer-const": ["warn", {
+      "destructuring": "any",
+      "ignoreReadBeforeAssign": false
+    }]
   },
 }
